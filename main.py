3d55a3ddb72bbc12a7007f9a8158fa009bc8ca90
Johannes Olafsson <67959441+JoeOlafs@users.noreply.github.com>
2021-01-29 13:55:41 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-"#games" 
 import random
-
-games = ('Icelandic','Casino','Reverse Casino','Dummy Poker','Drinking Game')
-print(games[random.randint(0,len(games)-1)])
+import Rules
+games = ("icelandic","casino","reverse casino","dummy poker","drinking game")
+num = random.randint(0, len(games)-1)
+print(games[num])
+if games[num]=="drinking game":
+	for x in Rules.rules:
+		print(x)
