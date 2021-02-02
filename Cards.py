b73214f5e344b615435ebfb6aa083a50946760ee
JoeOlafs <jaolafsson@hotmail.com>
2021-02-02 22:17:16 +0200

    work in progress

--- Cards.py
+++ Cards.py
@@ -1,19 +1,19 @@
 import random
 import Rules
 cards = [['A','2','3','4','5','6','7','8','9','10','J','Q','K'], ['A','2','3','4','5','6','7','8','9','10','J','Q','K'], ['A','2','3','4','5','6','7','8','9','10','J','Q','K'], ['A','2','3','4','5','6','7','8','9','10','J','Q','K']]
-
+suits = ['Hearts','Spades','Diamonds','Clubs']
 def deckShuffle():
 	count = 0
 	while count != 52:
 		ans = input()
 		if ans != 'exit':
-			num = random.randint(0, len(cards)-1)
-			suit = random.randint(0, len(cards[num])-1)
+			suit = random.randint(0, len(cards)-1)
+			num = random.randint(0, len(cards[suit])-1)
 			count += 1
-			print('Card: ' + cards[num][suit])
-			print('Count: ' + str(count))
-			Rules.PrintRule(cards[num][suit])
+			print('Card: ' + cards[suit][num] + ' of ' + suits[suit])
+			#print('Count: ' + str(count))
+			Rules.PrintRule(cards[suit][num])
 			#print(Rules.drinkingGameRules[rule])
-			del cards[num][suit]
+			del cards[suit][num]
 			
 deckShuffle()
